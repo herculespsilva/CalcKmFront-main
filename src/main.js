84c0b40b8e1c387e1fb4a6a7627e8b6090bd11cf
herculespsilva <herculespsilva@hotmail.com>
2021-12-02 03:54:05 +0000

    Implementando o Token

--- src/main.js
+++ src/main.js
@@ -4,9 +4,33 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://8080-salmon-wildfowl-1q6ae3na.ws-us20.gitpod.io/calculo_do_Km/';
+axios.defaults.baseURL = 'https://8080-azure-cat-wph75yex.ws-us20.gitpod.io/calculo_do_Km/';
 
 // axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+
+console.log(axios.defaults.baseURL);
+
+axios.interceptors.request.use((config) => {
+  if (store.state.token) {
+    config.headers.Authorization = store.state.token;
+  }
+  return config;
+});
+axios.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error) => {
+    console.log(axios.defaults.baseURL);
+    if (error.response.status === 403) {
+      alert("Não autorizado!");
+    } else if (error.response.status === 401) {
+      store.commit("logout");
+      router.push("/");
+    }
+    throw error;
+  }
+);
 
 Vue.config.productionTip = false
 
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="login">
+
+  <h2>Login</h2>
+
+  <p>
+    <label for="username">Usuário</label>
+    <input type="text" id="username" required autofocus v-model="username">
+  </p>
+
+  <p>
+    <label for="inputPassword">Senha</label>
+    <input type="password" id="inputPassword" required v-model="password">
+  </p>
+  <button type="submit">Ok</button>
+  </form>
+
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'login',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    ...mapActions(['login']),
+    auth () {
+      this.login({ usuario: this.username, senha: this.password })
+    }
+  }
+}
+</script>
